99f4996672d7cae81c35d9ff726ccbfeaf5ef1ba
Matthew <matthew_mcaleer@hotmail.com>
2022-05-23 13:28:18 +1000

    commit tree f5a9e3a3cdf063b4b39aeb6e45a882417f1566af

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(learned_knowledge)
+        @knowledge << learned_knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
